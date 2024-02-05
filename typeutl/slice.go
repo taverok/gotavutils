8098a181bf5041f3ecea3aab52377ab14cabfce1
Denis Tokarev <taverok@protonmail.com>
2024-02-05 21:31:04 +0300

    more functions

--- typeutl/slice.go
+++ typeutl/slice.go
@@ -1,5 +1,31 @@
 package typeutl
 
+func Transform[T, R any](ee []T, f func(T) R) []R {
+	var result []R
+	for _, e := range ee {
+		result = append(result, f(e))
+	}
+	return result
+}
+
+func Filter[T any](ee []T, f func(T) bool) []T {
+	var result []T
+	for _, e := range ee {
+		if f(e) {
+			result = append(result, e)
+		}
+	}
+	return result
+}
+
+func ToMap[K comparable, V any](ee []V, keyFunc func(V) K) map[K]V {
+	result := make(map[K]V)
+	for _, e := range ee {
+		result[keyFunc(e)] = e
+	}
+	return result
+}
+
 func Intersect[T comparable](a, b []T) []T {
 	aMap := make(map[T]bool)
 	var result []T
