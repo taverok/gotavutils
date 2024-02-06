e488ae4be9c8118b999f0520f01cc257daac68c4
Denis Tokarev <taverok@protonmail.com>
2024-02-06 11:18:57 +0300

    mdw

---
+++ rest/mdw/basic_auth.go
@@ -0,0 +1,18 @@
+package mdw
+
+import "net/http"
+
+func BasicAuth(name string, pass string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			username, password, ok := r.BasicAuth()
+			if ok && pass == password && name == username {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		})
+	}
+}
